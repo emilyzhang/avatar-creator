d5ef3585f5cb9dca0ee218f6f1e1359fa2db3ad2
Emily Zhang <emilyzhang@netlify.com>
2020-12-29 18:07:02 -0800

    feat: add color picker

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,30 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/hair1.png"> -->
   <header class="hello">avatar creator</header>
-  <Creator/>
+  <Creator />
 </template>
 
 <script>
-import Creator from './components/Creator.vue'
+import Creator from "./components/Creator.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Creator,
-  }
-}
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Spartan:wght@100;200;300;400;500;600&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Spartan:wght@100;200;300;400;500;600&display=swap");
 
 html,
 body {
   padding: 0;
   margin: 0;
-  font-family: 'Spartan', sans-serif, -apple-system, BlinkMacSystemFont, Segoe UI, Oxygen,
-    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  font-family: "Spartan", sans-serif, -apple-system, BlinkMacSystemFont,
+    Segoe UI, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+    sans-serif;
   background-image: url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/a63491be-b5f0-49b8-bcf0-baf70fc55e02/da7yz1u-90fcafd2-1da5-43a7-9270-dc6a48b1c5a1.png);
   background-repeat: no-repeat;
   background-attachment: fixed;
@@ -33,30 +34,59 @@ body {
   text-align: center;
   height: 100%;
 }
+
+button {
+  font-family: inherit;
+  font-size: 1em;
+  padding: 8px 8px;
+  border-radius: 0px;
+  margin: 10px;
+  /* background: #aee1e2; */
+  outline: none;
+  border: none;
+  color: #77a2bc;
+  box-shadow: 7px 6px 28px 1px #646292;
+}
+button:hover { 
+  color: black;
+}
+button:active {
+  transform: scale(0.98); 
+  box-shadow: 3px 2px 22px 1px #646292;
+  outline: none;
+}
+
 .hello {
   border: 2px dashed white;
   text-align: center;
   display: inline-block;
   margin: 50px auto;
   font-size: 50px;
-  color: #77A2BC;
+  color: #77a2bc;
   padding: 30px;
   text-shadow: 5 5 30px white;
   -webkit-background-clip: text;
   -moz-background-clip: text;
   background-clip: text;
-  -webkit-text-fill-color: transparent; 
+  -webkit-text-fill-color: transparent;
   -moz-text-fill-color: transparent;
-  background-image: linear-gradient(270deg, #646292, #AEE1E2, white, #AEE1E2, #646292);
+  background-image: linear-gradient(
+    270deg,
+    #646292,
+    #aee1e2,
+    white,
+    #aee1e2,
+    #646292
+  );
 }
 .hello:hover {
   border: 2px dashed white;
-  text-shadow: 0 0 30px #728CA7;
-  color: white; 
+  text-shadow: 0 0 30px #728ca7;
+  color: white;
   -webkit-background-clip: none;
   -moz-background-clip: none;
   background-clip: none;
-  -webkit-text-fill-color: white; 
+  -webkit-text-fill-color: white;
   -moz-text-fill-color: white;
   background-image: white;
 }
--- src/components/Creator.vue
+++ src/components/Creator.vue
@@ -1,31 +1,106 @@
 <template>
-  <div class="creator">
-    <canvas id="pixi"></canvas>
+  <div class="grid-container">
+    <div class="creator">
+      <canvas id="pixi"></canvas>
+    </div>
+    <div class="color-picker">
+    <div id="picker"></div>
+    </div>
+    <div class="select-color">
+      <div id="color-square"></div>
+      <button @click="select('hairColor')">hair color</button>
+      <button @click="select('eyeColor')">eye color</button>
+    </div>
   </div>
 </template>
 
 <script>
 import * as PIXI from "pixi.js";
 import { ColorOverlayFilter } from "@pixi/filter-color-overlay";
+import { PixiConsole, PixiConsoleConfig } from "pixi-console";
+import iro from '@jaames/iro';
 
+
+const avatar = {};
+let stage;
+let colorPicker;
+
+
+// const featuresEnum = Object.freeze({
+//   "face": 0,
+//   "eyebrows": 1,
+//   "hairColor": 2,
+//   "hair": 3,
+//   "eyeColor": 4,
+//   "eyeTop": 5,
+//   "eyeLight": 6,
+//   "mouth": 7,
+//   "nose": 8,
+// })
+
+const hexToRGB = (hex) =>
+  hex
+    .replace(
+      /^#?([a-f\d])([a-f\d])([a-f\d])$/i,
+      (m, r, g, b) => "#" + r + r + g + g + b + b
+    )
+    .substring(1)
+    .match(/.{2}/g)
+    .map((x) => parseInt(x, 16));
 
 export default {
   name: "Creator",
-  components: {
-  },
+  components: {},
   methods: {
+    select (selection) {
+      console.log(selection)
+      this.changeColor(selection);
+    },
+    changeColor(selectedFeature) {
+      if (selectedFeature) {
+      var hex = colorPicker.color.hexString;
+      console.log("HEX", hex)
+      const selectedColor = hexToRGB(hex);
+      console.log(selectedColor);
+      console.log("attempting to change color");
+      console.log("avatar", avatar);
+      if (!stage) {
+        console.log("stage not ready yet");
+      }
+      avatar[selectedFeature].filters = [new ColorOverlayFilter([selectedColor[0]/255, selectedColor[1]/255, selectedColor[2]/255])]
+      }
+    },
+    attachConsole(stage) {
+      // customize default values of PixiConsole
+      const consoleConfig = new PixiConsoleConfig();
+      consoleConfig.consoleWidth = 420;
+      consoleConfig.consoleHeight = 420;
+
+      let pixiConsole = PixiConsole.getInstance();
+      if (!PixiConsole.getInstance()) {
+        pixiConsole = new PixiConsole(consoleConfig);
+      }
+      stage.addChild(pixiConsole);
+    },
     drawPixi() {
       var canvas = document.getElementById("pixi");
-      console.log(canvas)
 
       const app = new PIXI.Application({
-        width: 444,
-        height: 444,
+        width: 420,
+        height: 420,
         antialias: true,
-        // transparent: true,
-        backgroundColor: "0xffffff",
+        transparent: true,
+        // backgroundColor: "0xffffff",
         view: canvas,
       });
+      stage = app.stage;
+      // this.attachConsole(stage)
+
+      // const renderer = PIXI.autoDetectRenderer(420, 420);
+      // var screenRect = new PIXI.Rectangle(0,0, renderer.width/renderer.resolution, renderer.height/renderer.resolution);
+      // stage.filterArea = screenRect;
+
+      const hairContainer = new PIXI.Container();
       const hair = new PIXI.Sprite.from("../assets/hair1.png");
       const nose = new PIXI.Sprite.from("../assets/nose0.png");
       const mouth = new PIXI.Sprite.from("../assets/mouth0.png");
@@ -35,33 +110,78 @@ export default {
       const eyebrows = new PIXI.Sprite.from("../assets/eyebrows0.png");
       const hairColor = new PIXI.Sprite.from("../assets/hair1color.png");
       const face = new PIXI.Sprite.from("../assets/face1.png");
-      face.filters = [new ColorOverlayFilter([1, 0.8, 0.7])];
+      face.filters = [new ColorOverlayFilter([1, 0.845, 0.7])];
       eyebrows.filters = [new ColorOverlayFilter([0, 0.8, 0.83])];
       hairColor.filters = [new ColorOverlayFilter([0, 0.8, 0.83])];
-      eyeColor.filters = [new ColorOverlayFilter([0, 0.18, 0.3])];
-      app.stage.addChild(face);
-      app.stage.addChild(eyebrows);
-      app.stage.addChild(hairColor);
-      app.stage.addChild(hair);
-      app.stage.addChild(eyeColor);
-      app.stage.addChild(eyeTop);
-      app.stage.addChild(eyeLight);
-      app.stage.addChild(mouth);
-      app.stage.addChild(nose);
+      eyeColor.filters = [new ColorOverlayFilter([0.2, 0.2, 0.25])];
+      hair.tint = 0xbeebee;
+      hairContainer.addChild(hairColor);
+      hairContainer.addChild(hair);
+      // console.log(hair);
+      // console.log(face);
+      stage.addChild(face);
+      stage.addChild(eyebrows);
+      stage.addChild(hairContainer);
+      stage.addChild(eyeColor);
+      stage.addChild(eyeTop);
+      stage.addChild(eyeLight);
+      stage.addChild(mouth);
+      stage.addChild(nose);
+      avatar.face = face;
+      avatar.eyebrows = eyebrows;
+      avatar.hair = hair;
+      avatar.hairColor = hairColor;
+      avatar.eyebrows = eyebrows;
+      avatar.eyeLight = eyeLight;
+      avatar.mouth = mouth;
+      avatar.nose = nose;
+      avatar.eyeColor = eyeColor;
     },
   },
 
   mounted() {
-    this.drawPixi()
+    this.drawPixi();
+    colorPicker = new iro.ColorPicker('#picker', {
+    // Set the size of the color picker
+    width: 300,
+    // Set the initial color to pure red
+    color: "#f00",
+    display: "inline-block",
+    borderColor: "#ffffff",
+    padding: 0,
+    borderWidth: 2,
+  });
   },
 };
 </script>
 
 <style>
+.grid-container {
+  display: grid;
+  grid-template-areas: "lside avatar colorpicker random rside";
+  grid-template-rows: auto;
+  grid-template-columns: 20px 425px 500px auto 20px;
+  grid-gap: 15px;
+  align-content: center;
+}
 .creator {
   border: 2px dashed white;
-  margin: auto;
-  padding: 4px;
-  width: 450px;
+  /* margin: auto 100px auto; */
+  /* padding: 4px; */
+  width: 420px;
+  grid-area: avatar;
+}
+.select-color {
+  border: 2px dashed white;
+  /* margin: auto auto 100px; */
+  /* padding: 4px; */
+  grid-area: random;
+}
+.color-picker {
+  border: 2px dashed white;
+  grid-area: colorpicker;
+}
+#picker {
+  margin: 20px;
 }
 </style>
